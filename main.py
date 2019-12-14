8170229e81220144584be1bfdc2c222c2dde2ee4
Nicolò Scudeller <nico.scudeller@gmail.com>
2019-12-14 19:28:32 +0100

    first argparse imoplementation, verbosity to be add

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from openairquality import get_quality
 
-pm10=get_quality()
-print("Rome has a pm10 value of {}".format(pm10))
-no2=get_quality(city="Bologna", parameter='no2')
-if no2==-1:
-    print("Could not find a value of no2 for Bologna")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("username", help="Username to log-in (password required)")
+parser.add_argument("-p", "--password",
+                    help="Password to log-in required", required=True)
+parser.add_argument("-c", "--city",
+                    help="Name of the European city to be sought",
+                    required=True)
+parser.add_argument("-m", "--mol",
+                    help="Polluting molecule required (default=pm10)",
+                    choices=list_csv('pypackage/parameters.csv'),
+                    default="pm10")
+parser.add_argument("-v", "--verbosity", help="Increase output verbosity",
+                    action="count")
+args = parser.parse_args()
+
+
+
 
 
