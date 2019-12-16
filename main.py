5e3d7cf3c258f516b384ffafa55777c46f5a0e04
Gap-98 <marco.bernardi@student.h-farm.com>
2019-12-16 23:51:38 +0100

    main login

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from database.dbmanager import DatabaseManager
 from request.openair_requestor import OpenAirRequestor
 from csv_util.csv_cache import CsvUtil
@@ -13,4 +14,15 @@ database_option.add_argument("-a", "--add", help="Add user", action="store_true"
 database_option.add_argument("-d", "--delete", help="Clean database", action="store_true")
 parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="store_true")
 args = parser.parse_args()
+manager = DatabaseManager()
+if args.p:
+    check = manager.check_for_username_correct(args.username, args.password)
+    if not check:
+        sys.exit("Sorry, user doesn't exist")
+elif args.add:
+    manager.save_new_username_correct(args.username, args.password)
+elif args.delete:
+    manager.clean_up()
+    sys.exit("Database cleaned")
 
+manager.close()
